571b2453bb4056a42cec84dd450ea309c353594b
Ben Miller <BenM@eras.co.uk>
2021-03-11 09:30:41 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eras.Public.IronPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
